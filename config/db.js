bfed8b1f180773d2ad2ae37430718979649280f8
Fred <fred.gerner@icloud.com>
2020-07-11 20:30:00 +1000

    db connection

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('db connected');
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+module.exports = connectDb;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDb();
 
 app.get('/', (req, res) => {
     res.send('Api running');
