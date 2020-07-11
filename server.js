aa4b6c673acc9971479855e8ea36c557d7835058
Fred <fred.gerner@icloud.com>
2020-07-11 21:07:55 +1000

    added routes

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ connectDb();
 
 app.get('/', (req, res) => {
     res.send('Api running');
-})
+});
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
