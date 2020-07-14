19fa5c704d73ab01562c773c29d78349b771d94b
Fred <fred.gerner@icloud.com>
2020-07-14 21:25:31 +1000

    user authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+
 const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
 
 //@route GET api/auth
-// @desc Test route
+// @desc Authenticate user and get token
 // @access Public
 router.get('/', auth, async (req, res) => {
     try {
@@ -15,5 +20,50 @@ router.get('/', auth, async (req, res) => {
         console.error(err.message);
         res.status(500).send('Server error');
     }
-})
+});
+
+
+//@route POST api/auth
+// @desc Register user
+// @access Public
+router.post('/', [
+        check('email', 'Use a valid email').isEmail(),
+        check('password', 'Password needs to bee entered').exists()
+    ], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        } else {
+            const {email, password} = req.body;
+            try {
+                //see if user exists
+                let user = await User.findOne({email});
+                if (!user) {
+                    return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
+                };
+
+                const isMatch = await bcrypt.compare(password, user.password);
+
+                if (!isMatch) {
+                    return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
+                };
+
+                //return web token
+                const payload = {
+                    id: user.id
+                }
+                jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 90000}, (err, token) => {
+                    if (err) throw err;
+                    res.json({token});
+                });
+
+                // return res.status(201).send('User created');
+            } catch (e) {
+                console.log(e.message);
+                return res.status(500).send('Server error');
+            }
+        }
+    }
+)
+
 module.exports = router;
--- routes/api/users.test.js
+++ routes/api/users.test.js
@@ -1,7 +1,16 @@
-import {describe, expect, it} from "@jest/globals";
+const request = require("supertest");
+const server = require('../../server');
+const faker = require('faker');
+
+const body = {
+    "name": "${faker.name()}"
+    // email: faker.internet.email(),
+    // password: faker.internet.password()
+}
 
 describe('Users', () => {
-    it('should have a POST users endpoint', function () {
-        expect(true).toEqual(true);
+    it('should have a POST users endpoint', async () => {
+        const response = await request(server).post('/api/users', body);
+        expect(body.status).toEqual(204);
     });
 })
