e89731d6c2f7594e13be4b415a9eca35a6d1ae7b
Fred <fred.gerner@icloud.com>
2020-07-16 22:38:33 +1000

    something got broken

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const connectDb = async () => {
         await mongoose.connect(db, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+            useCreateIndex: true,
+            useFindAndModify: false,
         });
         console.log('db connected');
     } catch (err) {
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -74,7 +74,19 @@ router.post('/', [
             }
         }
     }
-)
+);
+//@route GET api/profile
+// @desc Get all profiles
+// @access Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server error');
+    }
+})
 
 
 module.exports = router;
