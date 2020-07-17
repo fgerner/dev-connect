ebab7deb92ba7eb9444e0376b43d41bdbe7403af
Fred <fred.gerner@icloud.com>
2020-07-17 15:48:21 +1000

    fixed bug

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
     company: {type: String},
     website: {type: String},
     location: {type: String},
